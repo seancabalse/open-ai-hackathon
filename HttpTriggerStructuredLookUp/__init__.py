ccddcc20b4e53ef6c5a933e485e609bb1e8b779e
Sean Cabalse <sean.cabalse@globe.com.ph>
2023-08-09 05:09:55 +0000

    Intial commit

---
+++ HttpTriggerStructuredLookUp/__init__.py
@@ -0,0 +1,83 @@
+import logging
+
+import azure.functions as func
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+
+from langchain.vectorstores.azuresearch import AzureSearch
+
+from langchain.chat_models import AzureChatOpenAI
+
+
+def init_embeddings():
+    logging.info(f"init_embeddings")
+    vector_store_address: str = 'https://globecogse.search.windows.net'
+    # vector_store_password: str = 'Jk3aMWZNUeOdjiPZxDWgaWS4lXOY001YQcRTZXVUbZAzSeCiFO0l'
+    vector_store_password: str = 'JsKG2IrQ0IjDWB2vWcCt4sZTYK6mdjzIMc1dGMLtaJAzSeDkp8lI'
+
+    index_name: str = 'globevectorindex'
+
+    embeddings: OpenAIEmbeddings = OpenAIEmbeddings(
+            openai_api_type="azure",
+            openai_api_version="2023-03-15-preview",
+            openai_api_base="https://globeopenai.openai.azure.com/",
+            openai_api_key="314a741f06914946b61ff593a05f7b49",
+            chunk_size=1)
+    vector_store: AzureSearch = AzureSearch(
+        openai_api_type="azure",
+        openai_api_version="2023-03-15-preview",
+        openai_api_base="https://globeopenai.openai.azure.com/",
+        openai_api_key="314a741f06914946b61ff593a05f7b49",
+        azure_search_endpoint=vector_store_address,
+        azure_search_key=vector_store_password,
+        index_name=index_name,
+        embedding_function=embeddings.embed_query)
+    return embeddings, vector_store
+
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    logging.info(f'{context.function_directory}')
+    
+    prompt = req.params.get('prompt')
+    if not prompt:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            prompt = req_body.get('prompt')
+
+    if prompt:
+        
+        embeddings, vector_store = init_embeddings()
+        
+        model = AzureChatOpenAI(
+            openai_api_type="azure",
+            openai_api_version="2023-03-15-preview",
+            openai_api_base="https://globeopenai.openai.azure.com/",
+            openai_api_key="314a741f06914946b61ff593a05f7b49",
+            temperature=0.25,
+            deployment_name="gpt-35-turbo"
+        )
+        qa = RetrievalQA.from_llm(
+            llm=model,
+            retriever=vector_store.as_retriever(),
+            verbose=True
+        )
+
+        logging.info(f"qa: {qa}")
+        
+        result = qa.run(f"{prompt}")
+        
+        return func.HttpResponse(
+             result.content,
+             status_code=200
+        )
+        
+        
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
---
+++ HttpTriggerUnstructureLookUp/__init__.py
@@ -0,0 +1,106 @@
+import logging
+
+import azure.functions as func
+from sqlalchemy.engine import Engine, create_engine
+import pymysql
+from langchain.chains import create_sql_query_chain
+import mysql.connector
+
+from langchain.chat_models import AzureChatOpenAI
+from langchain.sql_database import SQLDatabase
+from langchain.schema import HumanMessage, SystemMessage 
+
+
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    logging.info(f'{context.function_directory}')
+    
+    logging.info("Printing file ----- HERE -----")
+    f = open(f'{context.function_directory}/DigiCertGlobalRootCA.crt.pem', "r")
+    logging.info(f.read())
+    
+    prompt = req.params.get('prompt')
+    if not prompt:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            prompt = req_body.get('prompt')
+
+    if prompt:
+        
+        db_user="glbadmin"
+        db_password="Globe2023"
+        db_host="globemysqlservers.mysql.database.azure.com"
+        db_name='ms_training_mysql'
+        open_api_type="azure"
+        open_api_version="2023-03-15-preview"
+        open_api_base="https://globeopenai.openai.azure.com/"
+        open_api_key="314a741f06914946b61ff593a05f7b49"
+        
+        ssl_args = {
+        "ssl_ca": f'{context.function_directory}/DigiCertGlobalRootCA.crt.pem'
+        }
+
+        logging.info("Creating engine...")
+        engine = create_engine("mysql+mysqlconnector://{db_username}:{db_pass}@globemysqlservers.mysql.database.azure.com:3306/ms_training_mysql".format(db_username=db_user, db_pass=db_password),
+            connect_args=ssl_args,
+            echo=True
+        )
+        
+        logging.info("Done creating enginee....")
+        
+        engine._connection_cls
+        
+        
+            
+        
+        logging.info(" Creating database...")
+        database = SQLDatabase(engine=engine)
+
+
+        logging.info(" Creating Model ----")
+        model = AzureChatOpenAI(
+            openai_api_type="azure",
+            openai_api_version="2023-03-15-preview",
+            openai_api_base="https://globeopenai.openai.azure.com/",
+            openai_api_key="314a741f06914946b61ff593a05f7b49",
+            temperature=0,
+            deployment_name="gpt-35-turbo",
+        
+         )
+    
+        logging.info("Creating sql query chain.....")
+        chain = create_sql_query_chain(model, database)
+        
+        logging.info("Chain invoke....")
+        result = chain.invoke({"question": prompt})
+        
+        logging.info("Result: {ci_result}".format(ci_result=result))
+        
+        prompt_response = database.run(result)
+        logging.info(f"Prompt response: {prompt_response}")
+        
+        model_response = model(
+        [
+        SystemMessage(content=f"""You are a database expert that answers the question: {prompt}.
+                       After running the query: {prompt}, you got the answer: {prompt_response}.
+                       Your task is give the answer in simple language."""),
+        HumanMessage(content=prompt)
+        ]
+        )
+        
+        logging.info(f"Model response: {model_response.content}")
+        
+        return func.HttpResponse(
+             model_response.content,
+             status_code=200
+        )
+        
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
